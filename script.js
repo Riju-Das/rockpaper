8ad85b28c0733b474d62431fe424d3224303f8eb
Riju Das <riju.das1910@gmail.com>
2024-09-27 03:33:13 +0530

    Commited

--- script.js
+++ script.js
@@ -1,69 +1,132 @@
+const rock= document.querySelector(".rock")
+const scissor= document.querySelector(".scissor")
+const paper=document.querySelector(".paper")
+const inputbox= document.querySelector(".inputbox")
+const humanoutput = document.querySelector("#humanoutput")
+const gameoutput= document.querySelector("#gameoutput")
+const computeroutput = document.querySelector("#computeroutput")
 
+
+function getComputerChoice(){
+
+    let a=Math.floor(Math.random()*3)
+    if(a==0){
+        return "rock";
+    }
+    else if(a==1){
+        return "paper";
+    }
+    else{
+        return "scissor";
+    }
+}
+
+
+
+
+function playround(humanschoice,computerchoice){
+    if((humanschoice=="rock" && computerchoice=="paper") || (humanschoice=="scissor" && computerchoice=="rock") || (humanschoice=="paper" && computerchoice=="scissor")  ){
+        roundresult =("You lose! "+ computerchoice+  " beats "+ humanschoice)
+        // computerscore+=1
+    }
+    else if((humanschoice=="rock" && computerchoice=="scissor") || (humanschoice=="paper" && computerchoice=="rock") || (humanschoice=="scissor" && computerchoice=="paper") ){
+        roundresult =("You Win! "+ humanschoice +" beats "+computerchoice)
+        // humanscore+=1
+    }
+    else{
+        roundresult= ("Its a draw! both chose "+ humanschoice)
+    }
+}
 function playgame(){
-    function getComputerChoice(){
-        let a=Math.floor(Math.random()*3)
-        if(a==0){
-            return "rock";
-        }
-        else if(a==1){
-            return "paper";
-        }
-        else{
-            return "scissor";
-        }
-    }
-    
-    function gethumanchoice(){
-        let a=prompt("Enter ur choice")
-        a=a.toLowerCase()
-        if(a=="rock"){
-            return "rock"
-        }
-        else if(a=="paper"){
-            return "paper"
-        }
-        else if(a=="scissor"){
-            return "scissor"
-        }
-        else{
-            return "Enter a valid choice"
-        }
-    }
-    
-    function playround(humanschoice,computerchoice){
-        if((humanschoice=="rock" && computerchoice=="paper") || (humanschoice=="scissor" && computerchoice=="rock") || (humanschoice=="paper" && computerchoice=="scissor")  ){
-            console.log("You lose! "+ computerchoice+  " beats "+ humanschoice)
-            computerscore+=1
-        }
-        else if((humanschoice=="rock" && computerchoice=="scissor") || (humanschoice=="paper" && computerchoice=="rock") || (humanschoice=="scissor" && computerchoice=="paper") ){
-            console.log("You Win! "+ humanschoice +" beats "+computerchoice)
-            humanscore+=1
-        }
-        else{
-            console.log("Its a draw! both chose "+ humanschoice)
-        }
-    }
-    const humanselection = gethumanchoice()
-    const computerselection= getComputerChoice()
 
+    console.log(humanchoice)
+    computerselection= getComputerChoice()
+    let humanselection = humanchoice
     playround(humanselection,computerselection)
-}
-let humanscore=0;
-let computerscore=0;
-playgame()
-playgame()
-playgame()
-playgame()
-playgame()
-console.log("The score is: ")
-console.log("human: "+ humanscore +" computer "+ computerscore)
-if(humanscore>computerscore){
-    console.log("The winner is the human")
+    
+    humanscreen();
+    computerscreen();
+    gamescreen();
 
 }
-else if(humanscore==computerscore){
-    console.log("The match is a draw!")
+function humanscreen(){
+
+    const youchose= document.createElement("p");
+    const choosing = document.createElement("p")
+    youchose.textContent = "You Chose:";
+    choosing.textContent= humanchoice;
+    youchose.setAttribute("class", "Choose")
+    choosing.setAttribute("class", "Choose")
+
+    humanoutput.appendChild(youchose)
+    humanoutput.appendChild(choosing)
 }
-else{
-    console.log("The winner is the computer")
+
+function computerscreen(){
+    const compchose= document.createElement("p");
+    const compchoosing = document.createElement("p")
+    compchose.textContent = "Computer Chose:";
+    compchoosing.textContent= computerselection;
+    compchose.setAttribute("class", "Choose")
+    compchoosing.setAttribute("class", "Choose")
+
+    computeroutput.appendChild(compchose)
+    computeroutput.appendChild(compchoosing)
+    console.log(computerselection)
 }
+
+function gamescreen(){
+    const gamechose= document.createElement("p");
+    const gamechoosing = document.createElement("p")
+    gamechose.textContent = "Results: "
+    gamechoosing.textContent= roundresult;
+    gamechose.setAttribute("class", "Choose")
+    gamechoosing.setAttribute("class", "Choose")
+
+    gameoutput.appendChild(gamechose)
+    gameoutput.appendChild(gamechoosing)
+}
+
+
+let computerselection;
+let humanchoice;
+
+inputbox.addEventListener("click", (event)=>{
+    humanoutput.innerHTML="";
+    computeroutput.innerHTML=""
+    gameoutput.innerHTML=""
+    
+    let target = event.target;
+    switch(target.id){
+        case "rock":
+            humanchoice= "rock";
+            break;
+        case "paper":
+            humanchoice= "paper";
+            break;
+        case "scissor":
+            humanchoice= "scissor";
+            break;                 
+    }
+    playgame()
+
+})
+
+
+
+
+
+// let humanscore=0;
+// let computerscore=0;
+// console.log("The score is: ")
+// console.log("human: "+ humanscore +" computer "+ computerscore)
+// if(humanscore>computerscore){
+//     console.log("The winner is the human")
+
+// }
+// else if(humanscore==computerscore){
+//     console.log("The match is a draw!")
+// }
+// else{
+//     console.log("The winner is the computer")
+// }
